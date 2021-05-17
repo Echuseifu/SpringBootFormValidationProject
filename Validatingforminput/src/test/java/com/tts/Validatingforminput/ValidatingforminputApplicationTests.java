033cd9673225d327e4b93040ee75d22b4e2b8c4f
Echuseifu <79155072+Echuseifu@users.noreply.github.com>
2021-05-17 16:00:03 -0400

    Add files via upload

---
+++ Validatingforminput/src/test/java/com/tts/Validatingforminput/ValidatingforminputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.Validatingforminput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatingforminputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
